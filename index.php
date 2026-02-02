ee78283c8e259e0231797e6d6bd019fe678c9f20
Gilbert2026 <gilbertsamuel12102007@gmail.com>
2026-02-02 08:20:50 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['history'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator Sederhana | UKK RPL 2025</title>
+    <title>Kalkulator Sederhana | UKK RPL 2026</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <style type="text/css">
@@ -138,7 +138,7 @@ if (!isset($_SESSION['history'])) {
    </div>
  </div>
 
- <p class="text-center">&copy; UKK RPL 2025 |  GILBERT SAMUEL  XI RPL 1</p>
+ <p class="text-center">&copy; UKK RPL 2026 |  GILBERT SAMUEL  XII RPL 1</p>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
 </body>
